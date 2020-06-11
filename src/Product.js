f86af9923988cfae242b03a2e47366276d4aa0a0
MartonCsizmazia <csmarton93@gmail.com>
2020-06-11 21:21:47 +0200

    table for products implemented

--- src/Product.js
+++ src/Product.js
@@ -1,5 +1,7 @@
 import React , {Component}from "react";
 import Menu from "./Menu";
+import './main.css';
+import './bootstrap.min.css';
 
 class Product extends Component {
 
@@ -23,10 +25,39 @@ class Product extends Component {
 
     render() {
         return (
-            <div>
-                <button onClick={this.props.renderMenu} >Menu</button>
-                {this.state.products.map(product => <div key={product.id}>{product.name}</div>)}
+        <div>
+            <button onClick={this.props.renderMenu} >Menu</button>
+            <div className="limiter">
+                <div className="container-table100">
+                    <div className="wrap-table100">
+                        <div className="table100 ver6 m-b-110">
+                            <table data-vertable="ver6">
+                                <thead>
+                                <tr className="row100 head">
+                                    <th className="column100 column1" data-column="column1"></th>
+                                    <th className="column100 column2" data-column="column2">Id</th>
+                                    <th className="column100 column4" data-column="column4">Price</th>
+                                    <th className="column100 column5" data-column="column5">Profit</th>
+                                    <th className="column100 column6" data-column="column6">Manufacturer</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                {this.state.products.map(product =>
+                                    <tr className="row100" key={product.id}>
+                                        <td className="column100 column1" data-column="column1">{product.name}</td>
+                                        <td className="column100 column1" data-column="column1">{product.id}</td>
+                                        <td className="column100 column1" data-column="column1">{product.price}</td>
+                                        <td className="column100 column1" data-column="column1">{product.profit}</td>
+                                        <td className="column100 column1" data-column="column1">{product.manufacturer}</td>
+                                    </tr>
+                                )}
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
             </div>
+        </div>
         );
     }
 }
