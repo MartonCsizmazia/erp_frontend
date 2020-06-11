1e0a1a718ab31736f6f15f875027c28928baae7b
MartonCsizmazia <csmarton93@gmail.com>
2020-06-11 12:42:56 +0200

    incoming deliveries displayed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import Menu from "./Menu";
 import Stock from "./Stock";
+import IncomingDeliveries from "./IncomingDeliveries";
 //import './App.css';
 
 class App extends Component{
@@ -19,21 +20,13 @@ class App extends Component{
             .then(response => response.json())
             .then(data => console.log(data))
             .then(data =>
-
             {
                 this.setState({
                     products : data
-                    })
                 })
+            })
     }
-
     */
-
-    renderStock = () =>{
-        this.setState({
-            switcher:1
-        })
-    }
 
     renderMenu = () =>{
         this.setState({
@@ -41,18 +34,39 @@ class App extends Component{
         })
     }
 
+    renderStock = () =>{
+        this.setState({
+            switcher:1
+        })
+    }
+
+    renderIncomingDeliveries = () =>{
+        this.setState({
+            switcher:2
+        })
+    }
 
     //{this.state.products.map(product => <div>{product.name}</div>)}
     render() {
         return (
             <div >
+                {this.state.switcher === 0 ?(
+                    <Menu renderStock={this.renderStock}
+                          renderIncomingDeliveries={this.renderIncomingDeliveries}
+                    />
+                ) : null}
+
                 {this.state.switcher === 1 ?(
                     <Stock renderMenu={this.renderMenu}/>
                 ) : null}
 
-                {this.state.switcher === 0 ?(
-                    <Menu renderStock={this.renderStock}/>
+                {this.state.switcher === 2 ?(
+                    <IncomingDeliveries renderMenu={this.renderMenu}/>
                 ) : null}
+
+
+
+
 
             </div>
 
---
+++ src/IncomingDeliveries.js
@@ -0,0 +1,34 @@
+import React , {Component}from "react";
+import Menu from "./Menu";
+
+class incomingDeliveries extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            incomingdeliveries: [],
+        }
+    }
+
+    async componentDidMount() {
+        const url = "http://localhost:8080/incoming-deliveries";
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data)
+        this.setState({incomingdeliveries: data});
+        console.log(this.state.incomingdeliveries)
+    }
+
+    render() {
+        return (
+            <div>
+                <button onClick={this.props.renderMenu} >myMenu</button>
+                {this.state.incomingdeliveries.map(incomingdelivery =>
+                    <div key={incomingdelivery.id}>{incomingdelivery.id}{incomingdelivery.incomingLineitems.map(lineitem =>
+                        <div key={lineitem.id}>product:{lineitem.product.name}   quantity:{lineitem.quantity}</div>)}</div>)}
+            </div>
+        );
+    }
+}
+
+export default incomingDeliveries;
