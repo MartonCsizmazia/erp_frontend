63ec89886f71c08b50020136d1f191a5e2456c2e
MartonCsizmazia <csmarton93@gmail.com>
2020-06-23 08:25:27 +0200

    input form designed

--- src/DeliveryOrder.js
+++ src/DeliveryOrder.js
@@ -1,16 +1,22 @@
 import React, {Component} from "react";
 
+import './App.css';
+
 class DeliveryOrder extends Component{
 
+    constructor(props){
+        super(props);
 
+    }
 
     render() {
         return (
             <div>
+                <button onClick={this.props.renderMenu} >Menu</button>
                 <form>
                     <label>
                         Name:
-                        <input type="text" name="name" />
+                        <input className="input-form" type="text" name="name" />
                     </label>
                     <input type="submit" value="Submit" />
                 </form>
